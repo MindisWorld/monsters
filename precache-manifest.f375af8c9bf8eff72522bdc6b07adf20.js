52e282bec36d36a1dbfae2f66ddac62281d658c9
Mindaugas <m.sandanavicius@gmail.com>
2020-08-04 01:19:44 +0300

    Updates

---
+++ precache-manifest.f375af8c9bf8eff72522bdc6b07adf20.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4b1f359e10cba37232a3d6f9ee633e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b35036aa8e552c143867",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d83666760140148fefb4",
+    "url": "/static/js/2.d96ee8a7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d96ee8a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b35036aa8e552c143867",
+    "url": "/static/js/main.96f900f7.chunk.js"
+  },
+  {
+    "revision": "242220f07351ba326e46",
+    "url": "/static/js/runtime-main.84b20e06.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
